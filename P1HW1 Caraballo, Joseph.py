545f926d04afe7f2c34d2bac89e7422c11ee1d61
carabalj3769 <carabalj3769@student.faytechcc.edu>
2024-06-20 21:36:51 +0000

    save!

--- P1HW1 Caraballo, Joseph.py
+++ P1HW1 Caraballo, Joseph.py
@@ -13,7 +13,7 @@ def main_exponents():
     result = exponentiate(base, exponent)
     print(f"{base} raised to the power of {exponent} is equal to {result}")
 
-    
+
 
 def calculate(starting_integer, integer_to_add, integer_to_subtract):
     result = starting_integer + integer_to_add - integer_to_subtract 
@@ -24,14 +24,13 @@ def main_add_and_sub():
 
     starting_integer = int(input("Enter a starting integer: "))
     integer_to_add = int(input("Enter a integer to add: "))
-    integer_to_subtrac = int(input("Enter a integer to subtract: "))
+    integer_to_subtract = int(input("Enter a integer to subtract: "))
 
     result = calculate("starting_integer; integer_to_add; integer_to_subtract")
 
     print(f"Enter Starting Integer: {starting_integer}")
     print(f"Enter Integer To Add: {integer_to_add}")
-    print(f"Enter Integer To Subtract: {integer_to_subtrac}")
-    print(f"{starting_integer} + {integer_to_add} - {integer_to_subtrac} is equal to {result}")
-
+    print(f"Enter Integer To Subtract: {integer_to_subtract}")
+    print(f"{starting_integer} + {integer_to_add} - {integer_to_subtract} is equal to {result}")
 if __name__ == "__main__": 
-    min()
+    main_add_and_sub()
