16364529bca6f43f9ba3460acaf0174fbe4b967a
carabalj3769 <carabalj3769@student.faytechcc.edu>
2024-07-08 17:19:26 +0000

    save

---
+++ P4Lab1 Caraballo, Joseph.py
@@ -0,0 +1,19 @@
+# Caraballo, Joseph
+# 8 July 2024
+# P4Lab1
+# Drawing shapes with Turtle graphic program with loops
+
+import turtle
+
+turtle.shape("turtle")
+turtle.forward(25)
+turtle.exitonclick()
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
